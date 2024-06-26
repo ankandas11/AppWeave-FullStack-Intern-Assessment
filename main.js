0733229704af394f8c4ccbfc41ef8594a1d975a3
Ankan Das <140325464+evuedeveloper@users.noreply.github.com>
2024-06-26 22:20:38 +0530

    update main.js

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-// const products = ./products.json
-import data from "./products.json" assert { type: 'json' };
-const products = data.products
-console.log(data)
-
-window.onload = function() {
 const openShopping = document.querySelector(".shopping"),
       body = document.querySelector("body"),
       list = document.querySelector(".list"),
       searchBar = document.getElementById("searchBar"),
       searchButton = document.getElementById("searchButton");
 
+let products = [
+    { "id": 1, "name": "Frock Women", "image": "1.png", "price": 2000, "type": "t-shirt", "color": "blue", "gender": "women", "quantity": 10 },
+    { "id": 2, "name": "Denim Jeans Men", "image": "2.png", "price": 2200, "type": "t-shirt", "color": "blue", "gender": "men", "quantity": 5 },
+    { "id": 3, "name": "Lehenga Women", "image": "3.png", "price": 2500, "type": "t-shirt", "color": "yellow", "gender": "women", "quantity": 11 },
+    { "id": 4, "name": "Saree Women", "image": "4.png", "price": 1800, "type": "t-shirt", "color": "red", "gender": "women", "quantity": 4 },
+    { "id": 5, "name": "T-Shirt Men", "image": "5.png", "price": 1000, "type": "t-shirt", "color": "red", "gender": "men", "quantity": 13 },
+    { "id": 6, "name": "Kurta Men", "image": "6.png", "price": 2600, "type": "t-shirt", "color": "yellow", "gender": "men", "quantity": 6 },
+    { "id": 7, "name": "T-Shirt Men", "image": "7.png", "price": 2400, "type": "t-shirt", "color": "green", "gender": "men", "quantity": 8 },
+    { "id": 8, "name": "Patiyala Women", "image": "8.png", "price": 1400, "type": "t-shirt", "color": "pink", "gender": "women", "quantity": 7 }
+];
 
 let listCards = JSON.parse(localStorage.getItem('cart')) || [];
 
@@ -25,19 +29,17 @@ const renderProducts = (products) => {
     products.forEach((value, key) => {
         let newDiv = document.createElement("div");
         newDiv.classList.add("col-md-4", "item");
-        let actionButton = value.quantity>0?`<button class="btn btn-primary" onclick="addToCart(${key})">Add To Cart</button>`:`<button class="btn btn-danger">Out Of Stock</button>`
         newDiv.innerHTML = `
             <div class="product-box">
                 <img src="img/${value.image}" class="img-fluid">
                 <div class="title">${value.name}</div>
                 <div class="price">${value.price.toLocaleString()}</div>
-                ${actionButton}
+                <button class="btn btn-primary" onclick="addToCart(${key})">Add To Cart</button>
             </div>
         `;
         list.appendChild(newDiv);
     });
 }
-window.renderProducts= renderProducts
 
 const filterProducts = () => {
     const query = searchBar.value.toLowerCase();
@@ -46,7 +48,6 @@ const filterProducts = () => {
     });
     renderProducts(filteredProducts);
 }
-window.filterProducts = filterProducts
 
 searchButton.addEventListener("click", filterProducts);
 
@@ -59,7 +60,6 @@ const filterBy = (criteria, value) => {
     }
     renderProducts(filteredProducts);
 }
-window.filterBy = filterBy
 
 //Adding to our cart
 
@@ -79,13 +79,10 @@ const addToCart = key => {
     localStorage.setItem('cart', JSON.stringify(listCards));
     updateCartQuantity();
 }
-window.addToCart = addToCart
 
 const updateCartQuantity = () => {
     const totalQuantity = listCards.reduce((sum, item) => sum + item.quantity, 0);
     document.querySelector(".quantity").innerText = totalQuantity;
 }
-window.updateCartQuantity = updateCartQuantity
 
 initApp();
-}
